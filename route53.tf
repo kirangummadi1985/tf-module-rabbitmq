a28a65bbbe0b9783a73422f5fcee99432125f590
kiran <kirangummadi1985@gmail.com>
2022-06-27 19:21:19 +0530

    Infra-setup

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "rabbitmq" {
   zone_id = var.PRIVATE_HOSTED_ZONE_ID
   name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
-  type    = "CNAME"
+  type    = "A"
   ttl     = "300"
   records = [aws_spot_instance_request.spot.private_ip]
 }
